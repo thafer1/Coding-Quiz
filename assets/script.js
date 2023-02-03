70e7ca4820a09d0de26b89f5ed2eae6f2e3dc58e
thafer1 <112597461+ZedZappie@users.noreply.github.com>
2023-02-03 13:11:20 -0500

    Added quiz questions to JS file

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var quizQuestions = [
+    {
+      title: 'Within an HTML page what element represents the visible content shown to the user?',
+      choices: ['<head>', '<body>', '<div>', '<html>'],
+      answer: '<body>',
+    },
+    {
+      title: 'A binary variable that has two possible values. Either “true” or “false.”',
+      choices: ['boolean', 'array', 'or operator', 'condition'],
+      answer: 'boolean',
+    },
+    {
+      title: 'What formatting would be best practice to make a variable for quiz answers?',
+      choices: ['quiz_answers', 'quiz.answers', 'answers4quiz', 'quizAnswers'],
+      answer: 'quizAnswers',
+    },
+    {
+      title: 'What element creates a connection to an external file',
+      choices: ['<a>', '<src>', '<link>', '<href>'],
+      answer: '<link>',
+    },
+    {
+      title: 'A single variable that is used to hold a set of data.',
+      choices: ['array', 'data.set', 'loop', 'database'],
+      answer: 'array'
+    }
+  ];
